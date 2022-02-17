8c4074d471bf1587463857b39161c596f70472df
Lil-Krish <kcbhatraju@gmail.com>
2022-02-16 20:20:05 -0500

    Game updates
    - time "game over"
    - bacteria counter
    - pausing added
    - "back" added

---
+++ Scripts/LevelTime.gd
@@ -0,0 +1,27 @@
+extends Label
+
+onready var timer = $LevelTimer
+
+func _ready():
+	timer.connect("timeout", self, "_on_Timer_timeout")
+	timer.start()
+
+func get_time(value):
+	var rounded = int(round(value))
+	return [floor(rounded/60), rounded-floor(rounded/60)*60]
+
+func _on_Score_level_up():
+	timer.stop()
+	timer.start()
+
+func _process(_delta):
+	var times = get_time(timer.get_time_left())
+	if times[1] < 10:
+		times[1] = "0"+str(times[1])
+	text = str("Timer: ", times[0], ":", times[1])
+
+func _on_Timer_timeout():
+	timer.stop()
+
+func _on_LoseTimer_timeout():
+	get_tree().change_scene("res://Scenes/MainMenu.tscn")
---
+++ Scripts/Score.gd
@@ -0,0 +1,41 @@
+extends Label
+
+
+signal level_up
+
+
+var score = 0
+
+# Points needed to level up
+var level_up_points = [3, 7, 12, 18, 25, 35, 50, 75, 100, 150, 200, 250, 300]
+
+func find_val(input, arr):
+	for val in arr:
+		if val > input:
+			return val
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Arena_point_gain():
+	score += 1
+	
+	var next_score = find_val(score, level_up_points)
+	
+	# Refresh the score counter
+	text = str("Score: ", score, "\nBacteria Until Next Level: ", next_score-score)
+	
+	# Check for a level up
+	if score in level_up_points:
+		level_up()
+
+
+func level_up():
+	emit_signal("level_up")
